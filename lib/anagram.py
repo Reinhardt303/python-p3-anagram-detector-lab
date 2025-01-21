aa9bf3256999078d8de7553006fc108f8033cbf4
Thomas <thomasrains01@gmail.com>
2025-01-20 20:34:39 -0800

    submits anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,27 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    @property
+    def word(self):
+        return self._word
+    
+    @word.setter
+    def word(self, word):
+        self._word = word
+
+    def match(self, possible_anagrams):
+        matches = []
+        word_list = [letter for letter in self.word]
+        for werd in possible_anagrams:
+            possible_anagram = [letter for letter in werd]
+            if sorted(possible_anagram) == sorted(word_list):
+                matches.append(werd)
+        return matches
+
+
+
+
+
+
